822541c82f9555a23a6e7db52c0f997b6a4e8ef7
Dawood ali <238x1a05a8@khitguntur.ac.in>
2025-09-13 22:54:32 +0530

    Create errorBoundary.tsx

---
+++ errorBoundary.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.error('ErrorBoundary caught:', error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div className="text-red-500">Something went wrong. Please refresh.</div>;
+    }
+
+    return this.props.children;
+  }
+}
