88903fc6cec09b869b98571edafc2321fc75315b
Dawood ali <238x1a05a8@khitguntur.ac.in>
2025-09-13 23:03:16 +0530

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,10 @@
+// page.tsx
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Welcome to Mini Buyer Lead Manager</h1>
+      <p><a href="/buyers">Go to Buyer List</a></p>
+      <p><a href="/buyers/new">Create New Lead</a></p>
+    </main>
+  );
+}
